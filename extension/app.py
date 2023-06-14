bb7cbc7ddf628b0187f287093cbb5798a367009a
Onyeka Okonta <101593852+OnyeEbube@users.noreply.github.com>
2023-06-14 10:03:44 +0100

    Update app.py and adding comments

--- extension/app.py
+++ extension/app.py
@@ -2,18 +2,24 @@ from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from functions import check_malicious
 
-
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/', methods=['POST'])
 def index():
-   data = request.get_json()
-   download_link = data.get('downloadLink')
-   is_malicious = check_malicious(download_link)
-   return jsonify({'is_malicious': is_malicious})
+    # Retrieve the JSON data from the request
+    data = request.get_json()
+    # Extract the download link from the data
+    download_link = data.get('downloadLink')
+    # Call the check_malicious function to determine if the link is malicious
+    is_malicious = check_malicious(download_link)
+    # Return the result as a JSON response
+    return jsonify({'is_malicious': is_malicious})
 
 def run_app():
+    # Run the Flask application in debug mode
     app.run(debug=True)
+
 if __name__ == '__main__':
+    # Start the Flask application
     run_app()
