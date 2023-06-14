c4e3f3750975cbb45129a08b660eddd59f384831
Onyeka Okonta <101593852+OnyeEbube@users.noreply.github.com>
2023-06-14 10:22:57 +0100

    Update script.js and adding comments

--- webapp/static/script.js
+++ webapp/static/script.js
@@ -2,11 +2,15 @@ document.addEventListener('DOMContentLoaded', function () {
     const loadingMessage = document.getElementById('loadingMessage');
     const form = document.querySelector('form');
 
+    // Add event listener to the form submit event
     form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        showLoadingMessage();
-});
-function showLoadingMessage() {
-    loadingMessage.style.display = 'block';
-  }
+        event.preventDefault(); // Prevent form submission
+
+        showLoadingMessage(); // Show loading message
+    });
+
+    // Function to show the loading message
+    function showLoadingMessage() {
+        loadingMessage.style.display = 'block'; // Display the loading message
+    }
 });
