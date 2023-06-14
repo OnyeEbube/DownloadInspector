1bc2aaa52542df62a152ac3d376a26bacc0731ec
Onyeka Okonta <101593852+OnyeEbube@users.noreply.github.com>
2023-06-14 10:15:29 +0100

    Update popup.js and adding comments

--- extension/popup.js
+++ extension/popup.js
@@ -1,18 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
+  // Get references to the necessary elements
   const form = document.getElementById('linkForm');
   const input = document.getElementById('link');
   const loadingMessage = document.getElementById('loadingMessage');
 
+  // Add event listener to the form submission
   form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const downloadLink = input.value;
+    event.preventDefault(); // Prevent the default form submission behavior
+    const downloadLink = input.value; // Get the value of the input field
 
-    showLoadingMessage();
+    showLoadingMessage(); // Show the loading message
 
+    // Send a message to the background script to check the malicious status of the download link
     chrome.runtime.sendMessage({ action: 'checkMalicious', downloadLink }, function (response) {
-      const isMalicious = response.isMalicious;
-      hideLoadingMessage();
+      const isMalicious = response.isMalicious; // Get the malicious status from the response
+      hideLoadingMessage(); // Hide the loading message
 
+      // Display an alert based on the malicious status
       if (isMalicious) {
         alert('The download link is malicious.');
       } else {
@@ -21,10 +25,12 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
+  // Show the loading message
   function showLoadingMessage() {
     loadingMessage.style.display = 'block';
   }
 
+  // Hide the loading message
   function hideLoadingMessage() {
     loadingMessage.style.display = 'none';
   }
